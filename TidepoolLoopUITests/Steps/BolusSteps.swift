179a905f14d82a7d9ecbfb19a4b0dcebfd6c5716
Scott Foster <148591275+Foscottl-TP@users.noreply.github.com>
2025-04-23 00:34:12 -0700

    [QAE-456] Simple Bolus Calculator test cases (#72)
    * LOOP-5200 Update
    
    Added updates to Bolus Steps and Home Steps. Created Simple Bolus feature file
    
    * More tests and bolus field function
    
    Adding test cases expanded within the scope of QAE-456 as they utilize previously committed functionality. Additionally added verification to bolus screen to read and confirm current value of Bolus field
    
    * Revert cleanup
    
    Just putting things back in their place.
    
    * Revert2.0
    
    Missed something
    
    * Remove "When I save and deliver and authenticate"
    
    Removed the function and updated the feature file and updated the feature file to the correct version (When I deliver and authenticate bolus)
    
    * GlucoseWarningExists update
    
    Updated to the changed name from the LoopUITestingKit changes
    
    * Set and read bolus screen text fields
    
    Set bolus screen values created in table form as suggested. The plan is to have this function apply to all versions of the bolus screen. Influenced feature files updated as a part of this change.
    
    * SpaceRemove
    
    Removed Space in Current Glucose column to match format of other like tables
    
    * Warning verification update
    
    Updated warning verification to apply to multiple warning types rather than just one. This may not scale with particular warnings however they will be updated as needed. Minor spacing and messaging fixes as well
    
    * [QAE-456] Formating fixes
    
    Addresses fixes requested in PR

--- TidepoolLoopUITests/Steps/BolusSteps.swift
+++ TidepoolLoopUITests/Steps/BolusSteps.swift
@@ -12,6 +12,7 @@ import XCTest
 
 func bolusSteps() {
     let bolusScreen = BolusScreen(app: app)
+    let therapySettingsScreen = TherapySettingsScreen(app: app)
     
     // MARK: Actions
     
@@ -19,12 +20,26 @@ func bolusSteps() {
         bolusScreen.tapCancelBolusButton()
     }
     
-    When("I set bolus value {float}") { matches, _ in
-        if bolusScreen.getBolusActionButtonLabel.contains("Enter Bolus") { bolusScreen.tapBolusActionButton() }
-        bolusScreen.tapBolusEntryTextField()
-        bolusScreen.clearBolusEntryTextField()
-        bolusScreen.setBolusEntryTextField(value: try String(matches.first(\.float)).replacing(",", with: "."))
-        bolusScreen.tapKeyboardDoneButton()
+    When("I set bolus screen values") { _, step in // Applies to Closed/Open Manual/Meal bolus screens. 
+        let bolusScreenMap = step.dataTable!.rows.map {
+            row -> (key: String, value: String) in (key: row[0], value: row[1])
+        }
+        for simpleTextField in bolusScreenMap {
+            switch simpleTextField.key {
+            case "Bolus":
+                bolusScreen.tapBolusEntryTextField()
+                bolusScreen.clearBolusEntryTextField()
+                bolusScreen.setBolusEntryTextField(value: simpleTextField.value)
+                bolusScreen.tapKeyboardDoneButton()
+            case "CurrentGlucose":
+                bolusScreen.tapCurrentGlucoseEntryTextField()
+                bolusScreen.clearCurrentGlucoseEntryTextField()
+                bolusScreen.setCurrentGlucoseEntryTextField(value: simpleTextField.value)
+                bolusScreen.tapKeyboardDoneButton()
+            default:
+                XCTFail("Unsupported field \(simpleTextField.key)")
+            }
+        }
     }
     
     When("I deliver and authenticate bolus") { _, _ in
@@ -40,5 +55,26 @@ func bolusSteps() {
     
     Then("bolus screen displays") { _, _ in
         XCTAssert(bolusScreen.bolusTitleExists)
+    }
+    
+    Then(/^warning title displays "?(.*?)"?$/) { matches, _ in
+        let expectedValue = String(matches.1)
+        let actualValue = therapySettingsScreen.getGuardrailWarningValue
+        XCTAssertEqual(expectedValue, actualValue)
+    }
+    
+    Then(/^(bolus|current glucose) field displays value "?(.*?)"?$/) { matches, _ in
+        let expectedValue = String(matches.2)
+        let actualValue = switch matches.1 {
+        case "bolus": bolusScreen.getBolusFieldValue()
+        case "current glucose": bolusScreen.getCurrentGlucoseFieldValue()
+        default: ""
+        }
+        if actualValue.isEmpty { XCTFail("Unsupported Field: \(matches.1)") }
+        XCTAssertEqual(
+            expectedValue,
+            actualValue,
+            "Comparison of expected `\(matches.1)` value `\(expectedValue)` does not match actual value `\(actualValue)`."
+        )
     }
 }
